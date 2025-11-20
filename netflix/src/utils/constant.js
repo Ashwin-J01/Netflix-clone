1106aeface9a13f0c25cabf84d7a764bcfb99ce4
Ashwin-J01 <ashwinj.23it@kongu.edu>
2025-11-20 22:32:34 +0530

    commit

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -4,5 +4,22 @@
 // Create React App requires env vars to be prefixed with REACT_APP_.
 const API_END_POINT = process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1";
 
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGJlZjViMDUzNWE3OGYwMjllMTQ0NDE5NTQ4MjM4MCIsInN1YiI6IjY1MDRhMjNkNTllOGE5MDExZWNhYTVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LTBaAb_2NPRGPr2HeGszyFDP-onLh-fiL7fzmnOFZUg'
+    }
+  };
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
+
 export { API_END_POINT };
 
